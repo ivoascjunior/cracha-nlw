8a13d6ef759ad70d2669a8ffac7995a2d13ccfbe
Ivo A. Júnior <ivoascjunior@gmail.com>
2021-10-24 02:19:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'ivoascjunior',
+  youtube: 'UCv9wtRqUa40qdbZ2ZtGGVUw',
+  facebook: 'ivoajunior9',
+  instagram: 'itsren.exe',
+  twitter: 'aktskren'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
